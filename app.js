feea1b90dc995e8912c23ffa2ae1315ed2dd67bb
Pony <ponyma2100@gmail.com>
2020-08-06 05:11:39 +0800

    feat: add method-override

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const Record = require('./models/record')
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 const app = express()
 const port = 3000
 const mongoose = require('mongoose')
@@ -22,6 +23,7 @@ db.once('open', () => {
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
   Record.find()
@@ -49,7 +51,7 @@ app.get('/records/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
-app.post('/records/:id/edit', (req, res) => {
+app.put('/records/:id', (req, res) => {
   const id = req.params.id
   const { name, category, date, amount } = req.body
   return Record.findById(id)
