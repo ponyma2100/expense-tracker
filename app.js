5e708d90d746eb4329d14569dbfe1d0ca6adc88b
Pony <ponyma2100@gmail.com>
2020-08-07 21:56:08 +0800

    feat: add category icon

--- app.js
+++ app.js
@@ -21,10 +21,13 @@ db.once('open', () => {
   console.log('mongodb conneted!')
 })
 
+
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(express.static('public'))
 
 Handlebars.registerHelper('setSelected', function (value, currentValue) {
   if (value === currentValue) {
@@ -34,11 +37,40 @@ Handlebars.registerHelper('setSelected', function (value, currentValue) {
   }
 })
 
+Handlebars.registerHelper('setIcon', function (category) {
+  if (category === '家居物業') {
+    return '<i class="fa fa-home"></i>'
+  } else if (category === '交通出行') {
+    return '<i class="fa fa-bus"></i>'
+  } else if (category === '休閒娛樂') {
+    return '<i class="fa fa-gamepad"></i>'
+  } else if (category === '餐飲食品') {
+    return '<i class="fa fa-coffee"></i>'
+  } else {
+    return '<i class="fa fa-surprise"></i>'
+  }
+})
+
+// Handlebars.registerHelper('getTotal', function (amount) {
+//   var total = fruits.reduce(function (a, b) { return a + b.number; }, 0);
+//   return total;
+//   }
+// })
+
+Handlebars.registerHelper('getTotal', function (amount) {
+  let totalAmount = amount.reduce(function (a, b) { return a + b.number; }, 0)
+  return totalAmount
+})
+
+
 
 app.get('/', (req, res) => {
   Record.find()
     .lean()
-    .then(records => res.render('index', { records }))
+    .then(records => {
+      const totalAmount = records.map(record => record.amount).reduce((acc, arr) => { return acc + arr })
+      res.render('index', { records, totalAmount })
+    })
     .catch(error => console.log(error))
 })
 
@@ -89,7 +121,10 @@ app.get('/filter', (req, res) => {
   // console.log(category)
   return Record.find({ category: category })
     .lean()
-    .then((records) => res.render('index', { records, category }))
+    .then((records) => {
+      const totalAmount = records.map(record => record.amount).reduce((acc, arr) => { return acc + arr })
+      res.render('index', { records, category, totalAmount })
+    })
     .catch(error => console.log(error))
 })
 
