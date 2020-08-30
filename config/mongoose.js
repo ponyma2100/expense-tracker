5caacfe2f67edcbf59f9f4f2aa3bbbd78cce67b2
Pony <ponyma2100@gmail.com>
2020-08-31 02:05:53 +0800

    modify mongoose setting

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,7 +3,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const mongoose = require('mongoose') // 載入mongoose
-const MONGODB_URI = process.env.MONGODB_URI
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/expense-tracker'
 // set connection to mongedb
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 const db = mongoose.connection
